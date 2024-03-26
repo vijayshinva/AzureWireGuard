8507934866f9c77abee722cacdbd276106f034aa
vc-vijaysh-karnure <vc.vijaysh.karnure@zee.com>
2024-03-26 11:43:32 +0530

    pihole install

--- AzureWireGuard/scripts/AzureWireGuard.sh
+++ AzureWireGuard/scripts/AzureWireGuard.sh
@@ -9,8 +9,41 @@ sed -i -e 's/#net.ipv4.ip_forward.*/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
 sed -i -e 's/#net.ipv6.conf.all.forwarding.*/net.ipv6.conf.all.forwarding=1/g' /etc/sysctl.conf
 sysctl -p
 
+## PiHole Config# Server Config
+mkdir /etc/pihole
+cat > /etc/pihole/setupVars.conf << EOF
+PIHOLE_INTERFACE=eth0
+QUERY_LOGGING=true
+INSTALL_WEB_INTERFACE=true
+LIGHTTPD_ENABLED=true
+INSTALL_WEB_SERVER=true
+DNSMASQ_LISTENING=local
+PIHOLE_DNS_1=1.1.1.1
+PIHOLE_DNS_2=1.0.0.1
+PIHOLE_DNS_3=2606:4700:4700::1111
+PIHOLE_DNS_4=2606:4700:4700::1001
+DNS_FQDN_REQUIRED=true
+DNS_BOGUS_PRIV=true
+DNSSEC=true
+TEMPERATUREUNIT=C
+WEBUIBOXEDLAYOUT=traditional
+API_EXCLUDE_DOMAINS=
+API_EXCLUDE_CLIENTS=
+API_QUERY_LOG_SHOW=all
+API_PRIVACY_MODE=false
+BLOCKING_ENABLED=true
+REV_SERVER=true
+REV_SERVER_CIDR=10.13.31.0/24
+REV_SERVER_TARGET=10.13.31.1
+REV_SERVER_DOMAIN=wireguard.vpn
+CACHE_SIZE=10000
+WEBTHEME=lcars
+EOF
+
+## Install PiHole
+curl -L https://install.pi-hole.net | bash /dev/stdin --unattended
+
 ## Install WireGurard
-apt-get update -y 
 apt-get install linux-headers-$(uname -r) -y
 apt-get install wireguard -y
 
@@ -77,7 +110,7 @@ PreDown = iptables -D FORWARD -i wg0 -j ACCEPT
 PreDown = iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
 PreDown = ip6tables -D FORWARD -i wg0 -j ACCEPT
 PreDown = ip6tables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
-DNS = 1.1.1.1
+DNS = 10.13.31.1
 
 [Peer]
 PublicKey =  $client_one_public_key
@@ -135,7 +168,7 @@ cat > /home/$2/wg0-client-1.conf << EOF
 [Interface]
 PrivateKey = $client_one_private_key
 Address = 10.13.31.101/32
-DNS = 1.1.1.1
+DNS = 10.13.31.1
 
 [Peer]
 PublicKey =  $server_public_key
@@ -152,7 +185,7 @@ cat > /home/$2/wg0-client-2.conf << EOF
 [Interface]
 PrivateKey = $client_two_private_key
 Address = 10.13.31.102/32
-DNS = 1.1.1.1
+DNS = 10.13.31.1
 
 [Peer]
 PublicKey =  $server_public_key
@@ -169,7 +202,7 @@ cat > /home/$2/wg0-client-3.conf << EOF
 [Interface]
 PrivateKey = $client_three_private_key
 Address = 10.13.31.103/32
-DNS = 1.1.1.1
+DNS = 10.13.31.1
 
 [Peer]
 PublicKey =  $server_public_key
@@ -186,7 +219,7 @@ cat > /home/$2/wg0-client-4.conf << EOF
 [Interface]
 PrivateKey = $client_four_private_key
 Address = 10.13.31.104/32
-DNS = 1.1.1.1
+DNS = 10.13.31.1
 
 [Peer]
 PublicKey =  $server_public_key
@@ -203,7 +236,7 @@ cat > /home/$2/wg0-client-5.conf << EOF
 [Interface]
 PrivateKey = $client_five_private_key
 Address = 10.13.31.105/32
-DNS = 1.1.1.1
+DNS = 10.13.31.1
 
 [Peer]
 PublicKey =  $server_public_key
@@ -220,7 +253,7 @@ cat > /home/$2/wg0-client-6.conf << EOF
 [Interface]
 PrivateKey = $client_six_private_key
 Address = 10.13.31.106/32
-DNS = 1.1.1.1
+DNS = 10.13.31.1
 
 [Peer]
 PublicKey =  $server_public_key
@@ -237,7 +270,7 @@ cat > /home/$2/wg0-client-7.conf << EOF
 [Interface]
 PrivateKey = $client_seven_private_key
 Address = 10.13.31.107/32
-DNS = 1.1.1.1
+DNS = 10.13.31.1
 
 [Peer]
 PublicKey =  $server_public_key
@@ -254,7 +287,7 @@ cat > /home/$2/wg0-client-8.conf << EOF
 [Interface]
 PrivateKey = $client_eight_private_key
 Address = 10.13.31.108/32
-DNS = 1.1.1.1
+DNS = 10.13.31.1
 
 [Peer]
 PublicKey =  $server_public_key
@@ -271,7 +304,7 @@ cat > /home/$2/wg0-client-9.conf << EOF
 [Interface]
 PrivateKey = $client_nine_private_key
 Address = 10.13.31.109/32
-DNS = 1.1.1.1
+DNS = 10.13.31.1
 
 [Peer]
 PublicKey =  $server_public_key
@@ -288,7 +321,7 @@ cat > /home/$2/wg0-client-10.conf << EOF
 [Interface]
 PrivateKey = $client_ten_private_key
 Address = 10.13.31.110/32
-DNS = 1.1.1.1
+DNS = 10.13.31.1
 
 [Peer]
 PublicKey =  $server_public_key
@@ -304,6 +337,8 @@ chmod go+r /home/$2/wg0-client-10.conf
 ## Firewall 
 ufw allow 51820/udp
 ufw allow OpenSSH
+ufw allow 53/tcp
+ufw allow 53/udp
 ufw disable
 ufw enable
 
